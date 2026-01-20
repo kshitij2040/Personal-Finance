0c763ca87cd475091399693c51e2c59aae355d32
Kshitij Mishra <kshitijmishra1302@gmail.com>
2026-01-20 16:21:24 +0530

    Add Vercel URL to CORS in common.py

--- config/settings/common.py
+++ config/settings/common.py
@@ -117,6 +117,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://personal-finance-xi-lake.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
